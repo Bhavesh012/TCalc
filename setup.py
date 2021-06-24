d6877e39cf06c47ab5e9d41a61751670fc0e0f4d
Bhavesh012 <rajputbhavesh04@gmail.com>
2021-06-24 20:25:20 +0530

    updated-setup.py-init.py

--- setup.py
+++ setup.py
@@ -1,6 +1,48 @@
 from setuptools import setup, find_packages
+import numpy as np
+import re
 
-setup(name="Telescope Calculator", 
-    version="1.0",
-    packages=find_packages()
+# auto-updating version code stolen from Orbitize
+def get_property(prop, project):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+                       open(project + '/__init__.py').read())
+    return result.group(1)
+
+def get_requires():
+    reqs = []
+    for line in open('requirements.txt', 'r').readlines():
+        reqs.append(line)
+    return reqs
+
+def get_extensions():
+    extensions = []
+    return extensions
+
+setup(
+    name='Telescope Calculator',
+    version=get_property('__version__'),
+    description='Telescope Calculator! A tool for your telescope',
+    url='https://github.com/Bhavesh012/Telescope-Calculator',
+    author='Bhavesh Rajpoot, Ryan Keenan, Binod Bhattarai, Dylon Benton',
+    author_email='',
+    license='MIT',
+    packages=find_packages(),
+    ext_modules=get_extensions(),
+    include_dirs=[np.get_include()],
+    include_package_data = True,
+    zip_safe=False,
+    classifiers=[
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Engeneering/Research',
+        'Topic :: Scientific/Engineering :: Astronomy',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+        ],
+    keywords='Telescopes Astronomy Eyepiece Magnification',
+    install_requires=get_requires()
     )
