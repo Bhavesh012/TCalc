080f8275f03f71b6f9df63a70ab5923776db7b66
Bhavesh012 <rajputbhavesh04@gmail.com>
2021-06-24 19:57:56 +0530

    updated

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -23,7 +23,7 @@ author = 'Bhavesh Rajpoot, Ryan Keenan, Binod Bhattarai, Dylon Benton'
 master_doc='index'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -47,7 +47,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'press'
+# html_theme = 'insegel'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/index.rst
+++ docs/index.rst
@@ -9,6 +9,9 @@ Welcome to Telescope-Calculator's documentation!
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
+   
+   telescope
+   functions
 
 
 
