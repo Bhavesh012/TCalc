1ad7bec935b2911228bd97ee6dbb05083b247737
R P Keenan <rpkeenan@email.arizona.edu>
2021-06-23 21:24:59 -0700

    removing some redundant files

--- ryan_mag.py
+++
@@ -1,5 +0,0 @@
-def compute_magnification(flength_telescope, flength_eyepiece):
-    """Compute the magnification of a telescope-eyepiece pair
-    """
-
-    return flength_telescope / flength_eyepiece
