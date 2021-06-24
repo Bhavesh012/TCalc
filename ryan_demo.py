5aaff367e52d98f2dccc43e833aef0fe71f55bdb
R P Keenan <rpkeenan@email.arizona.edu>
2021-06-23 21:24:16 -0700

    Updating main functionality

--- ryan_demo.py
+++ ryan_demo.py
@@ -1,11 +1,9 @@
-from ryan_telescope import eyepiece, telescope
+from telescope import eyepiece, telescope
 
 piece1 = eyepiece(5, 50)
 piece2 = eyepiece(1, 50)
 
-my_telescope = telescope(50,1000)
-
-my_telescope.list_eyepiece()
+my_telescope = telescope(50,300)
 
 my_telescope.add_eyepiece(piece1)
 my_telescope.add_eyepiece(piece2)
@@ -13,6 +11,11 @@ my_telescope.add_eyepiece(piece1,'copy of 1',select=False)
 
 my_telescope.list_eyepiece()
 
-print(my_telescope.magnification)
+my_telescope.say_configuration()
+
 my_telescope.select_eyepiece(0)
-print(my_telescope.magnification)
+my_telescope.say_configuration()
+
+my_telescope.show_resolving_power()
+my_telescope.show_magnification_limits()
+my_telescope.show_eyepiece_limits()
